83bd72761891e514c3ae9e67980d7f5cb51fad1d
DimaMalah <dmitryaronov77@gmail.com>
2022-09-15 12:58:57 +0300

    shifting added

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -16,10 +16,12 @@ export class FormComponent implements OnInit {
   url:string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx0mx3OYcD9Ge89SC942Y8dxDJrYwmfdjaqA&usqp=CAU'
   animation = ""
   animationState = ""
+  xShift = ""
+  yShift = ""
+
 
   getFile(event:any){
     let fileType = event.target.files[0].type
-    console.log(event.target.value);
     if(fileType.match(/image\/*/)){
       let reader = new FileReader()
       reader.readAsDataURL(event.target.files[0])
@@ -37,13 +39,27 @@ export class FormComponent implements OnInit {
     this.url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx0mx3OYcD9Ge89SC942Y8dxDJrYwmfdjaqA&usqp=CAU'
   }
 
-  animationType(value:any){
+  animationType(value:string){
     this.animation = value
     this.animationState = "start"
     setTimeout(()=>this.animationState="end", 0)
-    console.log(this.animation);
+  }
+
+  shiftX(val: HTMLInputElement, elem: any){
+    const style = elem.children[0].style
+    this.xShift = `${val.value}px`
+    style.position = 'relative'
+    style.left= this.xShift
+  }
+
+  shiftY(val: HTMLInputElement, elem: any){
+    const style = elem.children[0].style
+    this.yShift = `${val.value}px`
+    style.position = "relative"
+    style.bottom = this.yShift
 
   }
+
 
   ngOnInit(): void {
   }
